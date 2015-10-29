24ccaaa4df34cd646ffd8fde14e234e9ba28689e
Denton-L <liu.denton+github@gmail.com>
2015-10-29 12:31:56 -0400

    The BoosterPack LEDs work

--- LABryinth/led.c
+++ LABryinth/led.c
@@ -1,15 +1,17 @@
 #include <OrbitBoosterPackDefs.h>
 #include "Energia.h"
 
-static const int LEDPorts[] = {LED1Port, LED2Port, LED3Port, LED4Port};
+static const int LEDPORTS[] = {LED1Port, LED2Port, LED3Port, LED4Port};
 static const int LEDS[] = {LED1, LED2, LED3, LED4};
 
 void setup_led() {
 	int i;
-	for (i = 0; i < 4; i++)
-		  GPIOPinTypeGPIOOutput(LEDPorts[i], LEDS[i]);
+	for (i = 0; i < 4; i++) {
+		GPIOPadConfigSet(LEDPORTS[i], LEDS[i], GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+		GPIOPinTypeGPIOOutput(LEDPORTS[i], LEDS[i]);
+	}
 }
 
 void set_led(int led, char state) {
-	GPIOPinWrite(LEDPorts[led], LEDS[led], state ? HIGH : LOW);
+	GPIOPinWrite(LEDPORTS[led], LEDS[led], state ? LEDS[led] : LOW);
 }
