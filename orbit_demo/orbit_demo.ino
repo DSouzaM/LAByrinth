cafd460b4ba2102d4d321b418d70416e523c5e59
Denton-L <liu.denton+github@gmail.com>
2015-11-12 01:28:45 -0500

    Removed DeviceInit

--- orbit_demo/orbit_demo.ino
+++ orbit_demo/orbit_demo.ino
@@ -19,7 +19,6 @@ extern "C" {
 /* ------------------------------------------------------------ */
 /*				Forward Declarations							*/
 /* ------------------------------------------------------------ */
-void DeviceInit();
 void acceltest();
 
 char I2CGenTransmit(char * pbData, int cSize, bool fRW, char bAddr);
@@ -28,111 +27,12 @@ bool I2CGenIsNotIdle();
 
 void setup()
 {
-  DeviceInit();
+  OrbitOledInit();
 }
 
 void loop()
 {
   acceltest();
-}
-
-/* ------------------------------------------------------------ */
-/***	DeviceInit
- **
- **	Parameters:
- **		none
- **
- **	Return Value:
- **		none
- **
- **	Errors:
- **		none
- **
- **	Description:
- **		Initialize I2C Communication, and GPIO
- */
-void DeviceInit()
-{
-  /*
-   * First, Set Up the Clock.
-   * Main OSC		  -> SYSCTL_OSC_MAIN
-   * Runs off 16MHz clock -> SYSCTL_XTAL_16MHZ
-   * Use PLL		  -> SYSCTL_USE_PLL
-   * Divide by 4	  -> SYSCTL_SYSDIV_4
-   */
-  SysCtlClockSet(SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ | SYSCTL_USE_PLL | SYSCTL_SYSDIV_4);
-
-  /*
-   * Enable and Power On All GPIO Ports
-   */
-  //SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOA | SYSCTL_PERIPH_GPIOB | SYSCTL_PERIPH_GPIOC |
-  //						SYSCTL_PERIPH_GPIOD | SYSCTL_PERIPH_GPIOE | SYSCTL_PERIPH_GPIOF);
-
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOA );
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOB );
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOC );
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOD );
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOE );
-  SysCtlPeripheralEnable(	SYSCTL_PERIPH_GPIOF );
-  /*
-   * Pad Configure.. Setting as per the Button Pullups on
-   * the Launch pad (active low).. changing to pulldowns for Orbit
-   */
-  GPIOPadConfigSet(SWTPort, SWT1 | SWT2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
-
-  GPIOPadConfigSet(BTN1Port, BTN1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
-  GPIOPadConfigSet(BTN2Port, BTN2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
-
-  GPIOPadConfigSet(LED1Port, LED1, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
-  GPIOPadConfigSet(LED2Port, LED2, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
-  GPIOPadConfigSet(LED3Port, LED3, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
-  GPIOPadConfigSet(LED4Port, LED4, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
-
-  /*
-   * Initialize Switches as Input
-   */
-  GPIOPinTypeGPIOInput(SWTPort, SWT1 | SWT2);
-
-  /*
-   * Initialize Buttons as Input
-   */
-  GPIOPinTypeGPIOInput(BTN1Port, BTN1);
-  GPIOPinTypeGPIOInput(BTN2Port, BTN2);
-
-  /*
-   * Initialize LEDs as Output
-   */
-  GPIOPinTypeGPIOOutput(LED1Port, LED1);
-  GPIOPinTypeGPIOOutput(LED2Port, LED2);
-  GPIOPinTypeGPIOOutput(LED3Port, LED3);
-  GPIOPinTypeGPIOOutput(LED4Port, LED4);
-
-  /*
-   * Enable ADC Periph
-   */
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-
-  GPIOPinTypeADC(AINPort, AIN);
-
-  /*
-   * Enable ADC with this Sequence
-   * 1. ADCSequenceConfigure()
-   * 2. ADCSequenceStepConfigure()
-   * 3. ADCSequenceEnable()
-   * 4. ADCProcessorTrigger();
-   * 5. Wait for sample sequence ADCIntStatus();
-   * 6. Read From ADC
-   */
-  ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
-  ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
-  ADCSequenceEnable(ADC0_BASE, 0);
-
-  /*
-   * Initialize the OLED
-   */
-  OrbitOledInit();
-
-
 }
 
 void acceltest() {
@@ -214,13 +114,13 @@ void acceltest() {
     dataY = (rgchReadAccl2[2] << 8) | rgchReadAccl2[1];
     dataZ = (rgchReadAccl3[2] << 8) | rgchReadAccl2[1];
     
-     sprintf(printVal, "%4X, %4X, %4X", (int)dataX, (int)dataY, (int)dataZ);
+     sprintf(printVal, "%4d, %4d, %4d", (int)dataX, (int)dataY, (int)dataZ);
 
      OrbitOledSetCursor(0,0);
 
      OrbitOledPutString(printVal);
      OrbitOledUpdate();
-     delay(100);
+     delay(200);
      printVal[10] = {0};
      OrbitOledClear();
     
