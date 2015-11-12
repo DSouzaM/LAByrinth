b80d91a339f7edded92c42678dda66c3530ddbd1
jingyli <jingyi.li0310@gmail.com>
2015-11-12 11:35:42 -0500

    Adding classes

---
+++ LAByrinth/MapGenerator.c
@@ -0,0 +1,35 @@
+#include <math.h>
+
+//defines a numerical value for all elements within the maze
+const char SPACE = 0;
+const char WALL = 1;
+const char BALL = 2;
+const char PITFALL = 3;
+const char WIN_POS = 4;
+
+//defines the size of the screen
+const char WIDTH = 32;
+const char LENGTH = 128;
+
+char* map1(){
+    
+}
+
+char* map2(){
+    
+}
+
+char* map3(){
+    
+}
+
+
+char* mapGenerator (int level){
+    if (level == 0){
+        return map1();
+    }else if (level == 1){
+        return map2();
+    }else{
+        return map3();
+    }
+}
