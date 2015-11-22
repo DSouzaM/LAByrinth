98611f2acc94e7b91caae740e34ed0b311dc23a9
Denton-L <liu.denton+github@gmail.com>
2015-11-22 11:31:46 -0500

    Fixed the broken display bmp

--- LAByrinth/display.c
+++ LAByrinth/display.c
@@ -33,8 +33,7 @@ void put_string_display(const char* string, int x, int y) {
 
 void put_bitmap_display(const int bitmap[Y_MAX][X_MAX]) {
 	int i, j;
-	int *s;
-	int bmp[Y_MAX/8 * X_MAX] = {0};
+	char bmp[Y_MAX/8 * X_MAX] = {0};
 	for (i = 0; i < Y_MAX; i++) {
 		for (j = 0; j < X_MAX; j++) {
 			bmp[(i/8)*X_MAX + j] |= (bitmap[i][j] ? 1 : 0) << (i % 8);
